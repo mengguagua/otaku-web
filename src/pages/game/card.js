17a7611d56c3bf90a8a597d7435ce70604f1f32c
gaocc <15957108449@163.com>
2024-01-16 17:54:37 +0800

    fix 增加牌堆，护甲，手牌更新逻辑

--- src/pages/game/card.js
+++ src/pages/game/card.js
@@ -3,14 +3,14 @@ export let cardAll = {
     name: '剑',
     number: 4, // 数值
     element: 0, // 元素
-    type: 1, // 1攻 2守 3策略
+    type: 1, // 1攻 2守
     status: 1, // 1可使用 2不可使用
   },
   2: {
     name: '盾',
     number: 3,
     element: 0,
-    type: 2, // 1攻 2守 3策略
+    type: 2, // 1攻 2守
     status: 1,
   }
 };
@@ -18,6 +18,9 @@ export let cardAll = {
 let baseCard = [
   cardAll[1],
   cardAll[1],
+  cardAll[1],
+  cardAll[1],
+  cardAll[2],
   cardAll[2],
   cardAll[2],
 ];
