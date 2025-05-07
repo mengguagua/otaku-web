61f0a2d4e0457209e5d9127ac3d66754f57b0a54
gaocc <15957108449@163.com>
2025-05-07 09:39:04 +0800

    fix 首页动画效果继续开发

--- src/pages/animate/home/index.jsx
+++ src/pages/animate/home/index.jsx
@@ -1,9 +1,11 @@
 // animateJS, 网站首页
 import React, {useState, useEffect, useRef} from "react";
 import styles from './index.module.css';
-import {createDraggable, createScope, utils} from 'animejs';
+import {animate, createDraggable, createScope, utils} from 'animejs';
 
 let index = () => {
+  let [deltaYDistance, setDeltaYDistance] = useState(0);
+  let [middleRoadTextWidth, setMiddleRoadTextWidth] = useState(0);
   const containerRef = useRef(null);
   const animateRef = useRef(null);
   let draggableFirstScreen = useRef(() => {});
@@ -14,10 +16,17 @@ let index = () => {
     let totalDeltaY = 0;
     const handleWheel = e => {
       e.preventDefault();
-      console.log('wheel', e.deltaY); // 滚轮动作希望滚动的“距离”。正值时向下，负值时向上
+      // console.log('wheel', e.deltaY); // 滚轮动作希望滚动的“距离”。正值时向下，负值时向上
       totalDeltaY = totalDeltaY + e.deltaY;
-      console.log('totalDeltaY', totalDeltaY);
-      draggableFirstScreen.current.setX(totalDeltaY);
+      totalDeltaY = totalDeltaY > 0 ? totalDeltaY : 0
+      setDeltaYDistance(totalDeltaY + 80); // 添加中间横条的背景色。宽度额外加一段，防止移动时候不贴边
+      draggableFirstScreen.current.setX(totalDeltaY); // 设置横移位置
+      console.log('totalDeltaY---', totalDeltaY);
+      // 设置中间背景条上的文字左边距，值取css变量
+      const elem = document.documentElement; // 1、获取全局document元素
+      let cssVar = getComputedStyle(elem).getPropertyValue('--middle-road-text-left').trim(); // 2、获取css里的变量
+      cssVar = cssVar.slice(0, -2) // 去掉尾部的'px'
+      setMiddleRoadTextWidth(totalDeltaY + 80 - cssVar); // 设置具体距离
     };
     // 创建滚轮监听的对象
     // 加上 { passive: false } 才能阻止默认滚动
@@ -34,13 +43,22 @@ let index = () => {
     });
   }
 
+  let initRoad = () => {
+    animate(`.${styles['middle-road']}`,{
+
+    })
+  }
+
   return (
     <>
       <div  ref={animateRef} className={styles['top-layout']}>
         <div ref={containerRef} className={styles['first-screen']}>
-          <div className={styles['road']}/>
+          <div className={styles['black-area']}/>
+          <div className={styles['middle-road']} style={{width: deltaYDistance}}>
+            <div className={styles['middle-road-text']} style={{width: `${middleRoadTextWidth}px`, opacity: middleRoadTextWidth > 0 ? 1 : 0}}>Hello GAOCC</div>
+          </div>
           <div className={styles['notion-obj']}/>
-          <div className={styles['road']} style={{top: '60vh'}}/>
+          <div className={styles['black-area']} style={{top: '60vh'}}/>
         </div>
       </div>
     </>
